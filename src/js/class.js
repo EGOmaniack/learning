bc3eefd9635013dc22afb81e1a700ff4c29278cc
EGOMANIACK-PC\User <egomaniack@mail.ru>
2017-06-18 18:31:58 +1000

    Готово

---
+++ src/js/class.js
@@ -0,0 +1,91 @@
+class Task {
+    constructor({id, label, done}){
+        this.id = id;
+        this.label = label;
+        this.done = done;
+    }
+
+    ToggleDone(){
+        this.done = !this.done;
+    }
+}
+class List {
+    constructor(){
+        this.idCount = 0;
+        this.taskList = [];
+        this.domList = document.getElementById('list');
+        this.addInput = document.getElementById('addInput');
+        this.addButton = document.getElementById('addTodo');
+        this.addButton.onclick = ()=>{this.AddElement(this)};
+    }
+    toggleTaskDone(id){
+        this.taskList.forEach(task => {
+            if(task.id == id) {
+                task.ToggleDone();
+                this.render();
+            }
+        });
+    }
+    render(){
+        let listInner = '';
+        this.taskList.forEach((value, index) => {
+            listInner += `<div id="${
+                value.id
+            }" class="listItem"><input type="checkbox" class="p" ${
+                value.done ? 'checked' : ''
+            } /><div class="todo">${
+                value.label
+            }</div><div class="delete">x</div></div>`;
+        });
+        this.domList.innerHTML = listInner;
+    }
+    addTask(newLabel){
+        let newTask = new Task({id: newLabel.id, label: newLabel.label, done: newLabel.done});
+        this.taskList.push(newTask);
+    }
+    getTasks(thisList){
+        var request = new XMLHttpRequest();
+        request.open('GET', 'http://egomaniack.ru/lessons/ToDo/getToDo.php?nick=' + nick + '&list=false', true);
+        let self = this;
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                // Success!
+                let resp = request.responseText;
+                resp = JSON.parse(resp);
+                resp.forEach((task)=>{
+                    thisList.addTask(task);
+                });
+                thisList.render();
+            } else {
+                // We reached our target server, but it returned an error
+                return "error";
+            }
+        };
+        request.onerror = function() {
+            // There was a connection error of some sort
+            return "error";
+        };
+        request.send();
+    }
+    AddElement(){
+        if(this.addInput.value != ''){
+            this.addTask({id:this.idCount--, label:this.addInput.value, done: false});
+            this.render();
+            this.addInput.value = '';
+        }
+    }
+    save(thisList){
+        var request = new XMLHttpRequest(); //http://egomaniack.ru
+        request.open('POST', 'http://egomaniack.ru/lessons/ToDo/save.php', true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        request.send('input=' + JSON.stringify(thisList.taskList) + '&nick=' + nick + '&list=false');
+        request.onload = function(){
+            thisList.render(request.responseText);
+        }
+    }
+    delTask(id){
+        console.log(id);
+        this.taskList = this.taskList.filter(value => value.id != id);
+        this.render();
+    }
+}
---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+const domList = document.getElementById('list');
+const saveTodo = document.getElementById('saveTodo');
+
+domList.addEventListener('click', (e) => {
+    e.preventDefault();
+    switch (e.target.className) {
+        case "p":
+            list.toggleTaskDone(e.target.parentNode.id);
+            break;
+        case "todo":
+            list.toggleTaskDone(e.target.parentNode.id);
+            break;
+        case "delete":
+            list.delTask(e.target.parentNode.id);
+            break;
+    
+        default:
+            console.log("not recognized className -", e.target.className);
+            break;
+    }
+});
+const nick = "EGOmaniack";
+let list = new List();
+
+saveTodo.onclick = () => {
+    list.save(list);
+};
+
+list.getTasks(list);
+const addButton = document.getElementById('addTodo');
